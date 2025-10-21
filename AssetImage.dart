a63a22c1226232a59b16dab9b84c2d23c5a87077
DuyNguyen25092004 <121710594+DuyNguyen25092004@users.noreply.github.com>
2025-10-21 12:42:38 +0700

    Add AssetImage.dart file

---
+++ AssetImage.dart
@@ -0,0 +1,14 @@
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: 
+          Image(
+            image: AssetImage('/images/autorace.png')
+          )
+        ),
+      )
+    )
+  );
+  }
